a1acd14c0c37930296d52d94ce163214f7cd006c
Remy Bages <rbages@henix.fr>
2026-08-20 15:43:11 +0200

    bugfix accent et retour chariot

--- launch-doc.cmd
+++ launch-doc.cmd
@@ -3,7 +3,7 @@ chcp 65001 > nul
 cd /d "%~dp0"
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0launch-doc.ps1"
 
-:: Si une erreur survient, on garde la fenêtre ouverte
+:: Si une erreur survient, on garde la fenetre ouverte
 if %ERRORLEVEL% NEQ 0 (
     echo.
     echo Le script s'est arrete suite a une erreur.
